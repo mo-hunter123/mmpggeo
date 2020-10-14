4c3e67c11c9f2f53f24aed1bc0c2926132a030bc
mo-hunter123 <mohamed.makromi.2000@gmail.com>
2020-10-14 19:31:27 +0100

    database0

--- include/config.php
+++ include/config.php
@@ -3,9 +3,9 @@
     define('ROOT_URL', '');
 
     $host = "ec2-3-210-178-167.compute-1.amazonaws.com";
-    $username = "rrqmofbqylycgy";
+    $user = "rrqmofbqylycgy";
     $password = "acb65373986e7dd39c61b86bcc8de1251c81fe03277517ea43a65d4f112bf9be";
-    $database = "dbtum7vdulte6g";
+    $dbname = "dbtum7vdulte6g";
     $port = "5432";
 
     // $host = "localhost";
@@ -13,11 +13,21 @@
     // $password = "";
     // $database = "MMPG";
 
-    $con = mysqli_connect($host, $username, $password, $database, $port);
-
-    if(!$con){
-        die("connection failed: " . mysqli_connect_error());
-    }
+    try{
+        //Set DSN data source name
+          $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+      
+      
+        //create a pdo instance
+        $pdo = new PDO($dsn, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+      catch (PDOException $e) {
+      echo 'Connection failed: ' . $e->getMessage();
+      }
+    
 
     function TableSelect($con, $city){
         $sql = "SELECT * FROM Cities WHERE Cities.cityName = '$city' ";
