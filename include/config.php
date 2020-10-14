968c66d8296af4b9b48f724594065ae8abd1b4c8
mo-hunter123 <mohamed.makromi.2000@gmail.com>
2020-10-14 19:12:07 +0100

    database0

--- include/config.php
+++ include/config.php
@@ -1,10 +1,16 @@
 <?php
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "MMPG";
+    $host = "ec2-3-210-178-167.compute-1.amazonaws.com";
+    $username = "rrqmofbqylycgy";
+    $password = "acb65373986e7dd39c61b86bcc8de1251c81fe03277517ea43a65d4f112bf9be";
+    $database = "dbtum7vdulte6g";
+    $port = "5432";
 
-    $con = mysqli_connect($host, $username, $password, $database);
+    // $host = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $database = "MMPG";
+
+    $con = mysqli_connect($host, $username, $password, $database, $port);
 
     if(!$con){
         die("connection failed: " . mysqli_connect_error());
